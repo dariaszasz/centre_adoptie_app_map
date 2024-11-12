79c9fccc7e333a48dbc414796d30d533ce94caa9
iuliaseicean <iuliaseicean@yahoo.com>
2024-11-12 22:35:43 +0200

    clasa Freiwilliger reprezinta un voluntar din centrul de adoptie

---
+++ src/models/Freiwilliger.java
@@ -0,0 +1,30 @@
+package src.models;
+
+import java.util.List;
+
+public class Freiwilliger extends Person {
+    private String erfahrung;
+    private List<Tierheim> tierheime;
+
+    public Freiwilliger(int id, String name, String kontaktDetails, String erfahrung, List<Tierheim> tierheime) {
+        super(id, name, kontaktDetails);
+        this.erfahrung = erfahrung;
+        this.tierheime = tierheime;
+    }
+
+    public String getErfahrung() {
+        return erfahrung;
+    }
+
+    public void setErfahrung(String erfahrung) {
+        this.erfahrung = erfahrung;
+    }
+
+    public List<Tierheim> getTierheime() {
+        return tierheime;
+    }
+
+    public void setTierheime(List<Tierheim> tierheime) {
+        this.tierheime = tierheime;
+    }
+}
