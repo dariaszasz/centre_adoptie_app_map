294e1afa41662c8d91477904f55b03ef4384e8aa
iuliaseicean <iuliaseicean@yahoo.com>
2024-11-12 22:41:15 +0200

    clasa abstracta Person cu atribute comune precum id,name,kontaktdetails

---
+++ src/models/Person.java
@@ -0,0 +1,38 @@
+package src.models;
+
+public abstract class Person {
+    private int id;
+    private String name;
+    private String kontaktDetails;
+
+    public Person(int id, String name, String kontaktDetails) {
+        this.id = id;
+        this.name = name;
+        this.kontaktDetails = kontaktDetails;
+    }
+
+    // Getter und Setter
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKontaktDetails() {
+        return kontaktDetails;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setKontaktDetails(String kontaktDetails) {
+        this.kontaktDetails = kontaktDetails;
+    }
+}
