5e4f81357a5cc4364bf9895b0b8c0dec26f4307e
daria <szaszdaria2004@gmail.com>
2024-11-12 23:35:33 +0200

    Crearea meniului de client. Acesta are optiunea de a alege optiunea de a vizualiza toate animalele disponibile, adica neadoptate sau sa adopte un anumit animal dupa Id-ul acestuia

---
+++ src/presentation/ClientMenu.java
@@ -0,0 +1,45 @@
+package presentation;
+
+import controller.AnimalController;
+import models.Tier;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ClientMenu {
+    private AnimalController animalController;
+    private Scanner scanner;
+
+    public ClientMenu(AnimalController animalController) {
+        this.animalController = animalController;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void showMenu() {
+        while (true) {
+            System.out.println("\n Client Menu:");
+            System.out.println("1. Show all animals");
+            System.out.println("2. Adopt an animal");
+            System.out.println("0. Back to main menu");
+
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                List<Tier> animals = animalController.viewAllAnimals();
+                System.out.println("All animals: :");
+                for (Tier animal : animals) {
+                    System.out.println(animal);
+                }
+            } else if (choice == 2) {
+                System.out.println("ID of the animal you want to adopt: ");
+                int animalId = scanner.nextInt();
+                animalController.adoptAnimal(animalId);
+            } else if (choice == 0) {
+                break;
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+    }
+}
+
