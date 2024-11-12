2bf39dda72bccae37a69c318c169af56f1dd83a3
iuliaseicean <iuliaseicean@yahoo.com>
2024-11-12 22:54:04 +0200

    clasa care reprezinta tipul animalului

---
+++ src/models/Tierart.java
@@ -0,0 +1,38 @@
+package src.models;
+
+public class Tierart {
+    private int id;
+    private String artName;
+    private String besondereEigenschaften;
+
+    public Tierart(int id, String artName, String besondereEigenschaften) {
+        this.id = id;
+        this.artName = artName;
+        this.besondereEigenschaften = besondereEigenschaften;
+    }
+
+    // Getter und Setter
+    public int getId() {
+        return id;
+    }
+
+    public String getArtName() {
+        return artName;
+    }
+
+    public String getBesondereEigenschaften() {
+        return besondereEigenschaften;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setArtName(String artName) {
+        this.artName = artName;
+    }
+
+    public void setBesondereEigenschaften(String besondereEigenschaften) {
+        this.besondereEigenschaften = besondereEigenschaften;
+    }
+}
