8319a3df75e2f9b66bd91a1c8aa8588bb98c550c
iuliaseicean <iuliaseicean@yahoo.com>
2024-11-12 22:43:04 +0200

    clasa Pflegeprogramm defineste necesitatile de ingrijire ale animalului

---
+++ src/models/Pflegeprogramm.java
@@ -0,0 +1,38 @@
+package src.models;
+
+public class Pflegeprogramm {
+    private int id;
+    private String futterplan;
+    private String medizinischeVersorgung;
+
+    public Pflegeprogramm(int id, String futterplan, String medizinischeVersorgung) {
+        this.id = id;
+        this.futterplan = futterplan;
+        this.medizinischeVersorgung = medizinischeVersorgung;
+    }
+
+    // Getter und Setter
+    public int getId() {
+        return id;
+    }
+
+    public String getFutterplan() {
+        return futterplan;
+    }
+
+    public String getMedizinischeVersorgung() {
+        return medizinischeVersorgung;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFutterplan(String futterplan) {
+        this.futterplan = futterplan;
+    }
+
+    public void setMedizinischeVersorgung(String medizinischeVersorgung) {
+        this.medizinischeVersorgung = medizinischeVersorgung;
+    }
+}
