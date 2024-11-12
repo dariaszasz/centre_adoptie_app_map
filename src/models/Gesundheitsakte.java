93fb2c7a2efee5c7a8e0bb066f2f3858039e3263
iuliaseicean <iuliaseicean@yahoo.com>
2024-11-12 22:38:33 +0200

    clasa Gesundheitsakte contine informatii despre sanatatea animalului (diagnostice, tratamente, veterinar asociat)

---
+++ src/models/Gesundheitsakte.java
@@ -0,0 +1,50 @@
+package src.models;
+
+import java.util.List;
+
+public class Gesundheitsakte {
+    private int id;
+    private List<String> diagnosen;
+    private List<String> behandlungen;
+    private Tierarzt tierarzt;
+
+    public Gesundheitsakte(int id, List<String> diagnosen, List<String> behandlungen, Tierarzt tierarzt) {
+        this.id = id;
+        this.diagnosen = diagnosen;
+        this.behandlungen = behandlungen;
+        this.tierarzt = tierarzt;
+    }
+
+    // Getter und Setter
+    public int getId() {
+        return id;
+    }
+
+    public List<String> getDiagnosen() {
+        return diagnosen;
+    }
+
+    public List<String> getBehandlungen() {
+        return behandlungen;
+    }
+
+    public Tierarzt getTierarzt() {
+        return tierarzt;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDiagnosen(List<String> diagnosen) {
+        this.diagnosen = diagnosen;
+    }
+
+    public void setBehandlungen(List<String> behandlungen) {
+        this.behandlungen = behandlungen;
+    }
+
+    public void setTierarzt(Tierarzt tierarzt) {
+        this.tierarzt = tierarzt;
+    }
+}
