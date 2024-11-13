985e38cc53038cd5f2417cbd6a41363c9502d980
daria <szaszdaria2004@gmail.com>
2024-11-13 10:01:07 +0200

    actualizare meniu astfel incat clientii sa poata vedea toate detaliile animalului, inclusiv actele medicale si doctorul animalului.

--- src/presentation/ClientMenu.java
+++ src/presentation/ClientMenu.java
@@ -1,4 +1,3 @@
-/*
 package presentation;
 
 import controller.AnimalController;
@@ -6,6 +5,7 @@ import models.Tier;
 
 import java.util.List;
 import java.util.Scanner;
+
 
 public class ClientMenu {
     private AnimalController animalController;
@@ -15,10 +15,9 @@ public class ClientMenu {
         this.animalController = animalController;
         this.scanner = new Scanner(System.in);
     }
-
     public void showMenu() {
         while (true) {
-            System.out.println("\n Client Menu:");
+            System.out.println("\nClient Menu:");
             System.out.println("1. Show all animals");
             System.out.println("2. Adopt an animal");
             System.out.println("0. Back to main menu");
@@ -27,7 +26,7 @@ public class ClientMenu {
 
             if (choice == 1) {
                 List<Tier> animals = animalController.viewAllAnimals();
-                System.out.println("All animals: :");
+                System.out.println("Available animals:");
                 for (Tier animal : animals) {
                     System.out.println(animal);
                 }
@@ -42,56 +41,6 @@ public class ClientMenu {
             }
         }
     }
-}
 
- */
 
-package presentation;
-
-import controller.AnimalController;
-import models.Tier;
-
-import java.util.List;
-import java.util.Scanner;
-
-public class ClientMenu {
-    private AnimalController animalController;
-    private Scanner scanner;
-
-    public ClientMenu(AnimalController animalController) {
-        this.animalController = animalController;
-        this.scanner = new Scanner(System.in);
-    }
-
-    public void showMenu() {
-        while (true) {
-            System.out.println("\n Client Menu:");
-            System.out.println("1. Show all available animals for adoption");
-            System.out.println("2. Adopt an animal");
-            System.out.println("0. Back to main menu");
-
-            int choice = scanner.nextInt();
-
-            if (choice == 1) {
-                // Obține lista tuturor animalelor
-                List<Tier> animals = animalController.viewAllAnimals();
-
-                // Filtrează animalele neadoptate (status diferit de "adoptat")
-                System.out.println("Available animals for adoption:");
-                for (Tier animal : animals) {
-                    if (!"adopted".equalsIgnoreCase(animal.getStatus())) {
-                        System.out.println(animal);
-                    }
-                }
-            } else if (choice == 2) {
-                System.out.println("ID of the animal you want to adopt: ");
-                int animalId = scanner.nextInt();
-                animalController.adoptAnimal(animalId);
-            } else if (choice == 0) {
-                break;
-            } else {
-                System.out.println("Invalid choice.");
-            }
-        }
-    }
 }
